97ce77ae16df1f8ca1b3eeb3fdccb11a2593999a
javiducun <javiducun@gmail.com>
2023-05-20 10:50:07 +0200

    refactor for fizz case

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,8 +1,6 @@
 export const fizzBuzz = (num: number): string => {
-  if (num === 3) return "Fizz"
+  if (num % 3 === 0) return "Fizz"
   if (num === 5) return "Buzz"
-  if (num === 6) return "Fizz"
-  if (num === 9) return "Fizz"
   if (num === 10) return "Buzz"
   return num.toString()
 }
