a27bb83bc5f1751e4f0e4bee387e18ca0316388d
javiducun <javiducun@gmail.com>
2023-05-20 10:54:22 +0200

    refactor

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,8 +1,6 @@
 export const fizzBuzz = (num: number): string => {
   if (num === 15) return "FizzBuzz"
   if (num % 3 === 0) return "Fizz"
-  if (num === 5) return "Buzz"
-  if (num === 10) return "Buzz"
-  if (num === 20) return "Buzz"
+  if (num % 5 === 0) return "Buzz"
   return num.toString()
 }
