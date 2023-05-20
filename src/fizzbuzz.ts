f2019cb19ef1082090c5fcde5bcc960940b20d46
javiducun <javiducun@gmail.com>
2023-05-20 10:47:19 +0200

    add general case for numbers

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,9 +1,7 @@
 export const fizzBuzz = (num: number): string => {
-  if (num === 1) return "1"
-  if (num === 2) return "2"
   if (num === 3) return "Fizz"
-  if (num === 4) return "4"
   if (num === 5) return "Buzz"
   if (num === 6) return "Fizz"
-  return "Buzz"
+  if (num === 10) return "Buzz"
+  return num.toString()
 }
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -5,6 +5,8 @@ describe("FizzBuzz", () => {
     expect(fizzBuzz(1)).toEqual("1")
     expect(fizzBuzz(2)).toEqual("2")
     expect(fizzBuzz(4)).toEqual("4")
+    expect(fizzBuzz(7)).toEqual("7")
+    expect(fizzBuzz(8)).toEqual("8")
   })
   it("fizz buzz must return Fizz with 3", () => {
     expect(fizzBuzz(3)).toEqual("Fizz")
