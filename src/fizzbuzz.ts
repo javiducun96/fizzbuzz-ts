6bfb83fc3b1e5c80b7818dfc8b4d7b68dbb81488
javiducun <javiducun@gmail.com>
2023-05-20 10:42:12 +0200

    add case for 2

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,5 +1,6 @@
 export const fizzBuzz = (num: number): string => {
   if (num === 1) return "1"
+  if (num === 2) return "2"
   if (num === 3) return "Fizz"
   return "Buzz"
 }
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -1,15 +1,17 @@
 import { fizzBuzz } from "../src/fizzbuzz"
 
 describe("FizzBuzz", () => {
+  it("fizz buzz must return Fizz with 1", () => {
+    expect(fizzBuzz(1)).toEqual("1")
+  })
+  it("fizz buzz must return 2 with 2", () => {
+    expect(fizzBuzz(2)).toEqual("2")
+  })
   it("fizz buzz must return Fizz with 3", () => {
     expect(fizzBuzz(3)).toEqual("Fizz")
   })
 
   it("fizz buzz must return Buzz with 10", () => {
     expect(fizzBuzz(10)).toEqual("Buzz")
-  })
-
-  it("fizz buzz must return 1 with 1", () => {
-    expect(fizzBuzz(1)).toEqual("1")
   })
 })
